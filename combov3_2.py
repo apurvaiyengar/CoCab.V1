54581244d35fb400f0c0da61f5182f6a866723e6
apurvaiyengar <apurvaiyengar2@gmail.com>
2024-07-19 09:50:29 -0600

    debug cam.1

--- combov3_2.py
+++ combov3_2.py
@@ -85,7 +85,7 @@ def motor_on(rotations):
     GPIO.output(pin7, GPIO.HIGH)
     GPIO.setup(pin6, GPIO.OUT)
     GPIO.output(pin6, GPIO.LOW)
-    lin_count = 15
+    lin_count = 5
     total_count = lin_count * rotations
    
     i = 0
