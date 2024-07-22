b9638496cbfeec61c79e482f36697c0208b3d0e0
apurvaiyengar <apurvaiyengar2@gmail.com>
2024-07-22 13:32:04 -0600

    debug mot.2

--- combov3_3.py
+++ combov3_3.py
@@ -246,8 +246,7 @@ while True:
                     print("Good code!")
                     lin_forward()
                     time.sleep(1)
-                    print(stepper)
-                    stepper.motor_go(False, "1/4", 5, .01, False, .05)
+                    stepper.motor_go(False, "1/4", 10, .01, False, .05)
                     lin_reverse()
                     
                 else:
